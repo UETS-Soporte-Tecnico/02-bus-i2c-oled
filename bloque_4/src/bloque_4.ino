77eb2b899110e5c5062e4c3fbb38ca7c2ae3d32b
ruiz-martina <emily.ruizo.est@uets.edu.ec>
2026-09-12 05:46:17 +0000

    feat(bloque-04): reto integrador POST

--- bloque_4/src/bloque_4.ino
+++ bloque_4/src/bloque_4.ino
@@ -92,19 +92,18 @@ void runSystemPOST() {
     Serial.println("==========================================");
 
     // TODO 4.1: Mostrar la cabecera visual invocando showBootHeader()
-    /* ESCRIBE TU CÓDIGO AQUÍ */
+   showBootHeader()
 
     // TODO 4.2: Probar secuencialmente los 4 subsistemas usando logBoot(nombre, estado):
-    // 1. logBoot("ESP32 240MHz", true);
-    // 2. logBoot("I2C @ 400kHz", true);
-    // 3. logBoot("OLED 0x3C", true);
-    // 4. logBoot("Bateria 8.4V", true);
-    /* ESCRIBE TU CÓDIGO AQUÍ */
+     logBoot("ESP32 240MHz", true);
+     logBoot("I2C @ 400kHz", true);
+     logBoot("OLED 0x3C", true);
+     logBoot("Bateria 8.4V", true);
 
     delay(500);
 
     // TODO 4.3: Concluir la rutina mostrando la barra final con showSystemReady()
-    /* ESCRIBE TU CÓDIGO AQUÍ */
+   showSystemReady()
 }
 
 void setup() {
@@ -121,7 +120,12 @@ void setup() {
     // 2. Si total > 0 y la pantalla se inicializa con initDisplay():
     //    Ejecutar runSystemPOST();
     // 3. Si no, reportar falla por el Serial Monitor.
-    /* ESCRIBE TU CÓDIGO AQUÍ */
+    int total = scanI2CBus();
+    if (total > 0 && initDisplay()) {
+        runSystemPOST();
+    } else {
+        Serial.println("[ERROR] No se detectaron dispositivos I2C o la pantalla OLED falló.");
+    }
 }
 
 void loop() {
